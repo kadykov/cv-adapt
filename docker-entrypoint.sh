774e5b925ca268a68d7028a78a255dabe9456783
Aleksandr Kadykov <git@kadykov.com>
2025-02-06 22:29:02 +0000

    Comment out database initialization script in entrypoint; prepare for dependency updates in Dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,7 @@ done
 
 echo "PostgreSQL is up - initializing database..."
 cd /workspaces/cv-adapt/web-interface/backend
-python scripts/init_db.py
+# python scripts/init_db.py # TODO: uncomment this after moving dependencies to Dcokerfile
 
 echo "Starting application..."
 exec "$@"
